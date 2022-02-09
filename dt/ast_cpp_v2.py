4c60e66efbb3d11d6edc0fb81536067a62549875
Imara van Dinten <80925368+IvDinten@users.noreply.github.com>
2022-02-10 00:11:51 +0100

    AST with C++: Matches OVERLOADED_DECL_REF with INTEGER_LITERAL for usleep(4200) type of case.

--- dt/ast_cpp_v2.py
+++ dt/ast_cpp_v2.py
@@ -16,75 +16,32 @@ function_calls = []             # List of AST node objects that are function cal
 function_declarations = []      # List of AST node objects that are function declarations
 
 list_interest = ["usleep", "sleep"]
+dict_sleep = {}         # line number: usleep
+dict_sleep_dur = {}     # usleep: 4200
 
 file_name = os.path.join(pathlib.Path.home(), "Documents", "GitHub", "CPS_SPA_Detection_Tool", "tests", "ast_test_file.cpp")
 
 
-def new_traverse(node):
-    for child in node.get_children():
-        if child.location.line == 18:   # Currently hard-coded to check this line only.
-            print(f'FOUND {child.displayname} [line={child.location.line}, col={child.location.column}] type: {child.kind}')
-            if child.kind == clang.cindex.CursorKind.DECL_REF_EXPR:
-                print(f"\tchild {child.displayname} location line: {child.location.line}")
-                if child.referenced:
-                    print(f"\treferenced? {child.referenced.displayname} location line: {child.referenced.location.line}")
-            elif child.kind == clang.cindex.CursorKind.OVERLOADED_DECL_REF:
-                print(f"\tchild {child.displayname} location line: {child.location.line}")
-                if child.referenced:
-                    print(f"\treferenced? {child.referenced.displayname} location line: {child.referenced.location.line}")
-            elif child.kind == clang.cindex.CursorKind.INTEGER_LITERAL:
-                print(f"\tchild {child.displayname} location line: {child.location.line}")
-                if child.referenced:
-                    print(f"\treferenced? {child.referenced.displayname}; location line: {child.referenced.location.line}")
-
-                """ Get sleep duration """
-                # method 1
-                # for i in child.get_tokens():
-                #     print(f"\tdur: {i.spelling}")
-
-                # method 2
-                # gen = child.get_tokens()
-                # token = next(gen)
-                # print(f"\tdur: {token.spelling}")
-
-                # method 3
-                token_2 = next(child.get_tokens())
-                print(f"\tdur: {token_2.spelling}")
-
-            elif child.kind == clang.cindex.CursorKind.UNEXPOSED_EXPR:
-                print(f"\tchild {child.displayname} location line: {child.location.line}")
-                if child.referenced:
-                    print(f"\treferenced? {child.referenced.displayname}; location line: {child.referenced.location.line}")
-
-        new_traverse(child)
-
-
 # Traverse the AST tree
 def traverse(node):
-
-    # Recurse for children of this node
     for child in node.get_children():
+        if child.kind == clang.cindex.CursorKind.OVERLOADED_DECL_REF:
+            print(f'FOUND {child.displayname} [line={child.location.line}, col={child.location.column}] type: {child.kind}')
+            print(f"\tchild {child.displayname} location line: {child.location.line}")
+            if child.displayname in list_interest:
+                dict_sleep[child.location.line] = child.displayname
+        elif child.kind == clang.cindex.CursorKind.INTEGER_LITERAL:
+            print(f'FOUND {child.displayname} [line={child.location.line}, col={child.location.column}] type: {child.kind}')
+            print(f"\tchild {child.displayname} location line: {child.location.line}")
+
+            """ Get sleep duration """
+            token = next(child.get_tokens())
+            print(f"\tdur: {token.spelling}")
+
+            if child.location.line in dict_sleep.keys():
+                dict_sleep_dur[dict_sleep[child.location.line]] = token.spelling
+
         traverse(child)
-        # print(child.kind)
-        # for s_child in child.get_children():
-        #     print(f"-->{s_child.kind}")
-
-    # Add the node to function_calls
-    if node.type.kind == clang.cindex.CursorKind.CALL_EXPR:
-        function_calls.append(node)
-    #
-    # # Add the node to function_declarations
-    if node.type.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-        function_declarations.append(node)
-
-    # if node.type.kind == clang.cindex.CursorKind.INVALID:
-    #     print(f"found invalids: {node.displayname}")
-
-    # if node.type.kind == clang.cindex.CursorKind.DECL_STMT:
-    #     print(f'Found {node.displayname} [line={node.location.line}, col={node.location.column}]')
-
-    # Print out information about the node
-    print(f'Found {node.displayname} [line={node.location.line}, col={node.location.column}] type: {node.kind}')
 
 
 # Tell clang.cindex where libclang.dylib is
@@ -98,9 +55,7 @@ else:
     tu = index.parse(file_name)
 
     root = tu.cursor        # Get the root of the AST
-    # traverse(root)
-    new_traverse(root)
+    traverse(root)
 
-    # Print the contents of function_calls and function_declarations
-    # print(f"function calls: {function_calls}")
-    # print(function_declarations)
+    print("\n### Found matching results ###")
+    [print(key, ':', value) for key, value in dict_sleep_dur.items()]
