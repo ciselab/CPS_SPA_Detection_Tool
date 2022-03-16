3f2967a36b9c8d17d190e383c14c6ebf1ec8f975
Imara van Dinten <80925368+IvDinten@users.noreply.github.com>
2022-03-16 12:30:48 +0100

    Docker image is now built with baked-in local UID/GID to finally fix file permissions in the container. Resolves #23

--- docker_scripts/build-cps-spa-detection-tool.sh
+++ docker_scripts/build-cps-spa-detection-tool.sh
@@ -1,3 +1,4 @@
 # Build the new image from Dockerfile.cps-spa-detection-tool
 docker image build -t cps-spa-detection-tool \
+--build-arg LOCAL_UID="$(id -u)" --build-arg LOCAL_GID="$(id -g)" \
 $(pwd) -f Dockerfile.cps-spa-detection-tool
