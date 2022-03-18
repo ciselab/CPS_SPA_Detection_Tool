2272a3e07385291f28777b9bb0839d14126c32f9
Imara van Dinten <80925368+IvDinten@users.noreply.github.com>
2022-03-18 11:42:02 +0100

    Script to start the containers with the project and pattern input.

---
+++ run_analysis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+docker_scripts/run-cps-spa-detection-tool-container.sh AirSim mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh apollo mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh Arduino mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh Arduino-IRremote mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh ArduinoJson mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh ardumower mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh ardupilot mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh carla mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh CoppeliaSimLib mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh GAAS mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh openpilot mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh PiMower mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh PX4-Autopilot mwn
+#docker_scripts/run-cps-spa-detection-tool-container.sh qgroundcontrol mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh rfid mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh stofzuigerrobot mwn
+docker_scripts/run-cps-spa-detection-tool-container.sh turtlebot mwn
+
+docker_scripts/run-cps-spa-detection-tool-container.sh AirSim hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh apollo hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh Arduino hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh Arduino-IRremote hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh ArduinoJson hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh ardumower hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh ardupilot hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh carla hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh CoppeliaSimLib hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh GAAS hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh openpilot hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh PiMower hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh PX4-Autopilot hcft
+#docker_scripts/run-cps-spa-detection-tool-container.sh qgroundcontrol hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh rfid hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh stofzuigerrobot hcft
+docker_scripts/run-cps-spa-detection-tool-container.sh turtlebot hcft
