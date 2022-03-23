2a34b689451843185f6e1422f79aa4c18b5e2551
Imara van Dinten <80925368+IvDinten@users.noreply.github.com>
2022-03-23 20:42:14 +0100

    Combining pattern files (number of results) to one file.

---
+++ pattern_data_total.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Creates one file of all results files
+"""
+from os import walk
+from dt.utils.files import remove_file_if_exists
+
+results_path = "C:\\Users\\Imara\\Documents\\Server_results"
+f = []
+state = ""
+for (dir_path, dir_names, filenames) in walk(results_path):
+    for file in filenames:
+        file_full_path = str(dir_path) + "\\" + str(file)
+        f.append(file_full_path)
+
+remove_file_if_exists('pattern_data_total.csv')
+
+with open('pattern_data_total.csv', 'w', encoding="utf-8") as outfile:
+    for entry in f:
+        if "pattern_data" not in entry:
+            continue
+        if "final" in entry:
+            state = "\u25B2final"
+        elif "initial" in entry:
+            state = "\u25B2initial"
+        with open(entry, encoding="utf-8") as infile:
+            file_input = infile.read().rstrip("\n") + state
+            outfile.write(str(file_input))
+        outfile.write("\n")
