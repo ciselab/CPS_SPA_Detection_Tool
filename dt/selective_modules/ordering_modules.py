fcb794b1ab48b7d64d35663233276939e06de896
IvDinten <I.vanDinten@tudelft.nl>
2023-09-28 09:50:48 +0200

    Cleanup: removed unused code, fixed paths.

--- dt/selective_modules/ordering_modules.py
+++ dt/selective_modules/ordering_modules.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 import csv
-import os.path
 import re
 from os import path, walk
 import subprocess
 import json
-# from collections import Counter
 import dt.main as dt_main
 from typing import List, Dict
 import dt.selective_modules.results_part_one.analysis_results as ro
@@ -19,7 +17,7 @@ file_extensions: Dict[str, List[str]] = {
 
 
 def checkout_commit(dict_sel_commits: dict):
-    local = "/home/imara/GitHub/pxprojects/PX4-Autopilot/"
+    local = path.join(path.expanduser("~"), "GitHub", "pxprojects", "PX4-Autopilot")
     sel_modules = False     # Do not want to go through a pre-selection set of modules, this is handled differently.
     for each_hash in dict_sel_commits:
         print(f"{each_hash=}")
@@ -31,28 +29,6 @@ def checkout_commit(dict_sel_commits: dict):
         if 'hcft' in dict_sel_commits[each_hash]['antipatterns']:
             dt_main.main("PX4-Autopilot", "hcft", each_hash, sel_modules)
     print("Finished Detection Tool.")
-
-
-# def gathering_pxresults_list_aps():
-#     fieldnames = ['project', 'commit', 'hash', 'message', 'antipattern', 'keyword']
-#     # list_hash = []
-#     list_ap = set()
-#     with open(file, newline='') as f:
-#         reader = csv.DictReader(f, delimiter=',', quotechar='"', fieldnames=fieldnames)
-#         for row in reader:
-#             project = row['project'].strip()
-#             if project == "PX4-Autopilot":
-#                 antipattern = row['antipattern']
-#                 split_aps = antipattern.splitlines()
-#                 for each in split_aps:
-#                     list_ap.add(each)
-#                 # if "New:Hard-coded-timing" in antipattern or "New:Hard-coded-fine-tuning" in antipattern:
-#                 #     commit_hash = row['hash']
-#                 #     # hash_link = re.findall(r"\[[a-zA-Z0-9]+]\(([a-zA-Z0-9/.\-:=?]+)\)", commit_hash)
-#                 #     hash_code = re.findall(r"\[([0-9a-z]+)", commit_hash)
-#                 #     # print(f"{hash_code}")
-#                 #     list_hash.append(hash_code[0])
-#     return list_ap
 
 
 def gathering_pxresults():
@@ -67,10 +43,6 @@ def gathering_pxresults():
                 antipattern = row['antipattern']
                 split_antipattern = antipattern.splitlines()
                 for each in split_antipattern:
-                    # if "New:Hard-coded-timing" in each or "New:Hard-coded-fine-tuning" in each:
-                    #     commit_hash = row['hash']
-                    #     hash_code = re.findall(r"\[([0-9a-z]+)", commit_hash)
-                    #     set_hash.add(hash_code[0])
                     if "New:Hard-coded-timing" in each:
                         commit_hash = row['hash']
                         hash_code = re.findall(r"\[([0-9a-z]+)", commit_hash)
@@ -82,40 +54,16 @@ def gathering_pxresults():
     return set_hash_mwn, set_hash_hcft
 
 
-# def read_json():
-#     # json_file = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/results_px4.json"
-#     dir_path = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/px_commit_results"
-#     res = []
-#     for (dir_path, dir_names, file_names) in walk(dir_path):
-#         dir_file = file_names
-#         res.extend(dir_file)
-#     print(f"{res}")
-#
-#     list_files = []
-#
-#     for file_from_dir in res:
-#         file_path = os.path.join(dir_path, file_from_dir)
-#         with open(file_path) as f:
-#             data = json.loads(f.read())
-#             data_file = data['files']
-#             for i in data_file:
-#                 list_files.append(i['filename'])
-#     print(Counter(list_files).most_common())
-
-
 def read_json_commit_files():
-    # json_file = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/results_px4.json"
-    dir_path = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/px_commit_results"
+    dir_path = path.join(base_path, "px_commit_results")
     res = []
     for (dir_path, dir_names, file_names) in walk(dir_path):
         dir_file = file_names
         res.extend(dir_file)
 
-    # list_files = []
     dict_sha_files = {}
-
     for file_from_dir in res:
-        file_path = os.path.join(dir_path, file_from_dir)
+        file_path = path.join(dir_path, file_from_dir)
         with open(file_path) as f:
             data = json.loads(f.read())
             data_file = data['files']
@@ -124,14 +72,12 @@ def read_json_commit_files():
             found_cpp_type_file = False
             for i in data_file:
                 filename = i['filename']
-                _, filename_ext = os.path.splitext(filename)
+                _, filename_ext = path.splitext(filename)
                 if filename_ext.lower() in file_extensions["cpp"]:
                     found_cpp_type_file = True
-                # list_files.append(i['filename'])
                 commit_files.append(filename)
             if found_cpp_type_file:
                 dict_sha_files[sha_file] = {'commit_files': commit_files, 'antipatterns': []}
-    # print(Counter(list_files).most_common())
     return dict_sha_files
 
 
@@ -148,105 +94,39 @@ def added_aps_to_res(dict_commits, a_hash_mwn, a_hash_hcft, b_hash_hcft, b_hash_
     return dict_commits
 
 
-# def read_json_dates():
-#     # json_file = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/results_px4.json"
-#     dir_path = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/px_commit_results"
-#     res = []
-#     for (dir_path, dir_names, file_names) in walk(dir_path):
-#         dir_file = file_names
-#         res.extend(dir_file)
-#     print(f"{res}")
-#
-#     list_files = []
-#     dict_files = {}
-#
-#     for file_from_dir in res:
-#         file_path = os.path.join(dir_path, file_from_dir)
-#         with open(file_path) as f:
-#             data = json.loads(f.read())
-#             data_file = data['commit']['author']['date']
-#             list_files.append(data_file)
-#             dict_files[data_file] = data['sha']
-#             # print(data_file)
-#             # for i in data_file:
-#             #     print(i)
-#             #     list_files.append(i['date'])
-#     # print(Counter(list_files).most_common())
-#     list_files.sort()
-#     # print(list_files)
-#     # print(dict_files)
-#     for date in list_files:
-#         print(f"{date} : {dict_files[date]}")
-
-
-# def single_read_json(filename='src/modules/mavlink/mavlink_main.cpp'):
-#     # json_file = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/results_px4.json"
-#     dir_path = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/px_commit_results"
-#     res = []
-#     for (dir_path, dir_names, file_names) in walk(dir_path):
-#         dir_file = file_names
-#         res.extend(dir_file)
-#     print(f"{res}")
-#
-#     # list_files = []
-#
-#     for file_from_dir in res:
-#         file_path = os.path.join(dir_path, file_from_dir)
-#         with open(file_path) as f:
-#             data = json.loads(f.read())
-#             data_file = data['files']
-#             sha_current = data['sha']
-#             for i in data_file:
-#                 if filename in i['filename']:
-#                     print(sha_current)
-#     #             list_files.append(i['filename'])
-#     # print(Counter(list_files).most_common())
-
-
 def single_read_sha_json(sha_search):
-    # json_file = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/results_px4.json"
-    dir_path = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/px_commit_results"
+    dir_path = path.join(base_path, "px_commit_results")
     res = []
     for (dir_path, dir_names, file_names) in walk(dir_path):
         dir_file = file_names
         res.extend(dir_file)
-    # print(f"{res}")
-
-    # list_files = []
 
     for file_from_dir in res:
-        file_path = os.path.join(dir_path, file_from_dir)
+        file_path = path.join(dir_path, file_from_dir)
         with open(file_path) as f:
             data = json.loads(f.read())
-            # data_file = data['files']
             sha_current = data['sha']
             if sha_search == sha_current:
                 date_sha = data['commit']['author']['date']
-                # print(f"{sha_current} {date_sha}")
                 return date_sha
 
 
 def gather_hash_from_files():
-    filename_base = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/px_part2_aps/"
+    filename_base = path.join(base_path, "px_part2_aps")
     filename_one = "hcft_adjusted.txt"
     filename_two = "mwn_adjusted.txt"
     list_hcft = set()
     list_mwn = set()
     list_aps = [list_hcft, list_mwn]
     list_total_aps = set()
-    filename_list = [os.path.join(filename_base, filename_one), os.path.join(filename_base, filename_two)]
+    filename_list = [path.join(filename_base, filename_one), path.join(filename_base, filename_two)]
     for number, file_res in enumerate(filename_list):
         with open(file_res) as f:
             lines = f.readlines()
-            # total_commits_count = 0
             for line in lines:
                 hash_commit_pt = line.strip()
                 list_aps[number].add(hash_commit_pt)
                 list_total_aps.add(hash_commit_pt)
-                # print(hash_commit_pt)
-                # gather_files(hash_commit_pt)
-            # total_commits_count += total_commits_count+count
-    # print(f"Total number of commits to analyse (of part 2): {total_commits_count}")
     for each_hash in list_total_aps:
         gather_files(each_hash)
     return list_total_aps, list_hcft, list_mwn
@@ -278,42 +158,35 @@ def removing_files(dir_project, list_files_keep):
 
 
 def compare_lists():
-    dir_path = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/px_commit_results"
+    dir_path = path.join(base_path, "px_commit_results")
     res = []
     for (dir_path, dir_names, file_names) in walk(dir_path):
         dir_file = file_names
         res.extend(dir_file)
-    # print(f"{res}")
 
     dict_sha = {}
-
     for file_from_dir in res:
-        file_path = os.path.join(dir_path, file_from_dir)
+        file_path = path.join(dir_path, file_from_dir)
         with open(file_path) as f:
             data = json.loads(f.read())
             list_files = []
             data_file = data['files']
             for i in data_file:
                 data_filename = i['filename']
-                # list_files.append(data_filename.strip())
                 list_files.append(data_filename)
             dict_sha[data['sha']] = list_files
     print(dict_sha)
     print(f"{len(dict_sha)=}")
 
-    file_results_hcft_mwn = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/results_px4_server/PX4-Autopilot/results_hcft_mwn_px4.csv"
+    file_results_hcft_mwn = path.join(base_path, "results_px4_server", "PX4-Autopilot", "results_hcft_mwn_px4.csv")
 
-    # remove_keys = []
     remove_keys = set()
-
     with open(file_results_hcft_mwn) as fr:
         lines = fr.readlines()
         for line in lines:
             for key in dict_sha:
                 for each in dict_sha[key]:
                     if each == line.strip():
-                        # print(key)
-                        # remove_keys.append(key)
                         remove_keys.add(key)
     print(remove_keys)
 
@@ -332,76 +205,55 @@ def compare_lists():
 
 
 def check_line_file_commit(lines_lf, dict_sha, commit):
-    # print(f"{dict_sha[commit]=}")
-    # print(f"{commit=}")
-    # print(f"{lines_lf=}")
     for each_file in dict_sha[commit]:
         each_file_adjusted = "./"+each_file
-        # print(each_file_adjusted)
-        # print(lines_lf)
         if each_file_adjusted in lines_lf:
             return commit, dict_sha[commit]
     return None
 
 
 def final_compare_lists():
-    dir_path = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/px_commit_results"
+    dir_path = path.join(base_path, "px_commit_results")
     res = []
     for (dir_path, dir_names, file_names) in walk(dir_path):
         dir_file = file_names
         res.extend(dir_file)
-    # print(f"{res}")
 
     dict_sha = {}
-
     for file_from_dir in res:
-        file_path = os.path.join(dir_path, file_from_dir)
+        file_path = path.join(dir_path, file_from_dir)
         with open(file_path) as f:
             data = json.loads(f.read())
             list_files = []
             data_file = data['files']
             for i in data_file:
                 data_filename = i['filename']
-                # list_files.append(data_filename.strip())
                 list_files.append(data_filename)
             dict_sha[data['sha']] = list_files
-    # print(dict_sha)
-    # print(f"{len(dict_sha)=}")
-    # This is a dict with all commits, including non-C++ files projects
 
     dict_considered_commits = {}
-
-    # dir_path_list_files = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/list_files_keep_v3.txt"
-    dir_path_list_files = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/results_px4_server/PX4-Autopilot/files_in_project.txt"
+    dir_path_list_files = path.join(base_path, "results_px4_server", "PX4-Autopilot", "files_in_project.txt")
     with open(dir_path_list_files) as lf:
         lines_lf = lf.readlines()
         list_llf = []
         for llf in lines_lf:
             list_llf.append(llf.strip())
         for each_commit_all in dict_sha:
-            # print(f"{dict_sha[each_commit_all]=}")
-            # print(f"{lines_lf=}")
             comparison_res = check_line_file_commit(list_llf, dict_sha, each_commit_all)
             if comparison_res is not None:
                 comparison_res_hash, comparison_res_files = comparison_res
-                # print(f"{comparison_res=}")
-                # print(f"{comparison_res_hash=} {comparison_res_files=}")
                 dict_considered_commits[comparison_res_hash] = comparison_res_files
     print(f"{dict_considered_commits=}")
 
-    file_results_hcft_mwn = "/home/imara/GitHub/CPS_SPA_Detection_Tool/dt/selective_modules/results_px4_server/PX4-Autopilot/results_hcft_mwn_px4.csv"
+    file_results_hcft_mwn = path.join(base_path, "results_px4_server", "PX4-Autopilot", "results_hcft_mwn_px4.csv")
 
-    # remove_keys = []
     remove_keys = set()
-
     with open(file_results_hcft_mwn) as fr:
         lines = fr.readlines()
         for line in lines:
             for key in dict_considered_commits:
                 for each in dict_considered_commits[key]:
                     if each == line.strip():
-                        # print(key)
-                        # remove_keys.append(key)
                         remove_keys.add(key)
     print(remove_keys)
 
@@ -435,25 +287,11 @@ def main():
     for each in list_hash:
         gather_files(each)
 
-    # [OPTIONAL] Ordering from most common to the least common (s1p1)
-    # read_json()
-
-    # [OPTIONAL] Print hash for single file. (s1p1)
-    # single_read_json()
-
-    # [OPTIONAL] Print ordering by date. (s1p1)
-    # read_json_dates()
-
-    # [OPTIONAL] Print out all occurring aps (s1p1)
-    # results = gathering_pxresults_list_aps()
-    # print(results)
-
-    # [OPTIONAL] gather files from file of hashes, study 1 part 2
+    # Gather files from file of hashes, study 1 part 2
     # includes the gather_files() function
     res_list_total_aps, res_list_hcft, res_list_mwn = gather_hash_from_files()
     print(f"Number of unique commits to analyse (of part 2): hcft={len(res_list_hcft)} mwn={len(res_list_mwn)}")
     print(f"Total number of unique commits to analyse (of part 2): {len(res_list_total_aps)}")
-
 
     # nr.6) commit hash with files list, and at least 1 C++ type file in commit.
     res_hash_files = read_json_commit_files()
