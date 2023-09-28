12d88260dc8cd14b2840f9c1fb6c441ead3c7bfa
IvDinten <I.vanDinten@tudelft.nl>
2023-09-28 15:26:24 +0200

    Renamed docker files.

--- docker_scripts/build-cps-spa-detection-tool-studyOne.sh
+++
@@ -1,4 +0,0 @@
-# Build the new image from Dockerfile.cps-spa-detection-tool-studyOne
-docker image build -t cps-spa-detection-tool-studyOne \
---build-arg LOCAL_UID="$(id -u)" --build-arg LOCAL_GID="$(id -g)" \
-"$(pwd)" -f Dockerfile.cps-spa-detection-tool-studyOne
--- docker_scripts/run-cps-spa-detection-tool-container-studyone.sh
+++ docker_scripts/run-cps-spa-detection-tool-container-studyone.sh
@@ -8,4 +8,4 @@ docker run --rm -dit \
 --mount type=bind,source="$(pwd)/results",target=/home/user/cps-spa-detection-tool/results \
 --mount type=bind,source="$HOME/projects",target=/home/user/projects \
 -u "$(id -u)":"$(id -g)" \
-cps-spa-detection-tool-studyOne "$@"
+cps-spa-detection-tool-studyone "$@"
